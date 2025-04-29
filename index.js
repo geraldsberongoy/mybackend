72225af5ee22c61ccf13f6cb707b37235665b8d5
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-04-29 12:44:35 +0800

    style: format code for consistency and readability

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 // Simple Route Testing
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
-
+app.get("/api", (req, res) => {
+  res.send("API is working!");
+});
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
